526b97f3b558a2a4117df69846542ed03a24d2e4
umucci <118204872+umucci@users.noreply.github.com>
2026-06-16 13:02:11 -0300

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS requests (
+  id TEXT PRIMARY KEY,
+  title TEXT NOT NULL,
+  type TEXT NOT NULL,
+  details TEXT,
+  requested_by TEXT,
+  likes INTEGER NOT NULL DEFAULT 0,
+  created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
